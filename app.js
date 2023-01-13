3ba6266c7c94e46c29a6668f783295d84eb8aa7b
Diana-Sh1 <dianaa15052015@gmail.com>
2023-01-13 21:40:50 +0300

    made header

--- app.js
+++ app.js
@@ -1,58 +1,65 @@
-
-
-document.querySelector('body').addEventListener('mousemove', eyeball);
-function eyeball() {
-    let eye = document.querySelectorAll('.eye');
-    eye.forEach(function (eye){
-        let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
-        let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
-        let radian = Math.atan2(event.pageX - x, event.pageY - y);
-        let rot = (radian * (180 / Math.PI) * -1) + 270;
-        eye.style.transform = "rotate("+ rot +"deg)";
-    })
-}
-//BEE
-// document.querySelector('body').addEventListener('mousemove', rotateBee);
-// function randomSize (min,max) {
-//     min = Math.ceil(60);
-//     max = Math.ceil(500);
-//     return Math.floor(Math.random() * (max - min)) + min;
-// }
-//
-//
-// let bee = document.querySelectorAll('.bee');
-// for (let i = 0; i < bee.length; i++) {
-//     bee[i].style.left = Math.floor(Math.random()*99) + 'vw';
-//     bee[i].style.top = Math.floor(Math.random()*99) + 'vh';
-//     bee[i].style.width = randomSize() + 'px';
-// }
-//
-// function rotateBee() {
-//     bee.forEach(function (bee){
-//         let x = (bee.getBoundingClientRect().left) + (bee.clientWidth / 2);
-//         let y = (bee.getBoundingClientRect().top) + (bee.clientHeight / 2);
-//         let radian = Math.atan2(event.pageX - x, event.pageY - y);
-//         let rot = (radian * (180 / Math.PI) * -1) + 270;
-//         bee.style.transform = "rotate("+ rot +"deg)";
-//     })
-// }
+import { SplitText } from "/plugin/splitText.js";
 gsap.registerPlugin(Observer);
-function rotate(value) {
-    gsap.to(".face", {
-        rotate: value + '_short',
-        ease: 'back.out',
-        duration: 0.2,
-        overwrite: true,
-        transformOrigin: 'center center'
-    })
-}
-Observer.create ({
-    target: window,
-    type: "wheel, touch, scroll, pointer",
-    onUp: () => {rotate(0)},
-    onDown: () => {rotate(180)},
-    onLeft: () => {rotate(270)},
-    onRight: () => {rotate(90)},
+const bowl = document.querySelector(".bowl");
+const text = document.querySelector(".text");
+const addNoodlesBtn = document.getElementById("add-noodles");
+const chars = new SplitText(text, { type: "chars", charsClass: "char" });
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+gsap.defaults({ overwrite: true });
+
+function getRandomInt(val) {
+    return Math.ceil(Math.random() * val) * (Math.round(Math.random()) ? 1 : -1);
+}
+
+function addLetter() {
+    const el = document.createElement("div");
+    const char = alphabet[Math.floor(Math.random() * alphabet.length)];
+    const pos = 40;
+    const translate = 30;
+    const rotate = 60;
+
+    el.innerText = char;
+    el.classList.add("char");
+    el.style.top = `${getRandomInt(pos)}%`;
+    el.style.left = `${getRandomInt(pos)}%`;
+
+    bowl.appendChild(el);
+    gsap.to(el, {
+        xPercent: () => getRandomInt(translate),
+        yPercent: () => getRandomInt(translate),
+        rotation: () => getRandomInt(rotate),
+        ease: "expo.out",
+        duration: 2
+    });
+}
+
+function moveChars(obj) {
+    const { event, deltaX, deltaY } = obj;
+    const el = event.target;
+    const r = el.getBoundingClientRect();
+    const y = event.clientY - (r.top + Math.floor(r.height / 2));
+    const t = 5;
+
+    gsap.to(obj.event.target, {
+        xPercent: `+=${deltaX * t}`,
+        yPercent: `+=${deltaY * t}`,
+        rotation: `-=${deltaX * t * Math.sign(y)}`,
+        duration: 3,
+        ease: "expo.out"
+    });
+}
+
+Observer.create({
+    target: bowl,
+    onMove: (self) => self.event.target.matches(".char") && moveChars(self)
 });
 
+gsap.to(".char", {
+    xPercent: () => getRandomInt(10),
+    yPercent: () => getRandomInt(10),
+    rotation: () => getRandomInt(20),
+    duration: 5
+});
+
+addNoodlesBtn.addEventListener("click", addLetter);
