e1c31ab5b8d973108f5cafdc60f2dd6f6d07dad9
Diana-Sh1 <dianaa15052015@gmail.com>
2023-01-31 15:59:09 +0300

    matter-js

--- app.js
+++ app.js
@@ -1,4 +1,279 @@
-gsap.registerPlugin(ScrollTrigger);
+
+//HELLO
+const THICCNESS = 60;
+const SVG_PATH_SELECTOR = ".matter-path";
+const SVG_WIDTH_IN_PX = 100;
+const SVG_WIDTH_AS_PERCENT_OF_CONTAINER_WIDTH = 0.1;
+
+const matterContainer = document.querySelector("#matter-container");
+
+// module aliases
+var Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite,
+    Composites = Matter.Composites,
+    Common = Matter.Common,
+    Body = Matter.Body,
+    Svg = Matter.Svg,
+    Vector = Matter.Vector,
+    Vertices = Matter.Vertices,
+    Constraint = Matter.Constraint;
+
+// create an engine
+var engine = Engine.create();
+
+// add bodies
+
+
+// create a renderer
+var render = Render.create({
+    element: matterContainer,
+    engine: engine,
+    options: {
+        width: matterContainer.clientWidth,
+        height: matterContainer.clientHeight,
+        background: "transparent",
+        wireframes: false,
+        showAngleIndicator: false,
+
+    }
+});
+
+
+createSvgBodies();
+createCircle();
+/////////////////////
+// let boxA  = Bodies.rectangle(window.innerWidth / 2, window.innerHeight / 2, 20, 100, {
+//
+//             friction: 0.3,
+//             frictionAir: 0.00001,
+//             restitution: 0.8,
+//             isStatic: true,
+//             render: {
+//                 fillStyle: "#F99fff",
+//                 strokeStyle: "#F99fff"
+//             }
+//         });
+// let boxB  = Bodies.rectangle(200, 100, 50, 20, {
+//             friction: 0.3,
+//             frictionAir: 0.00001,
+//             restitution: 0.8,
+//             render: {
+//                 fillStyle: "#F99fff",
+//                 strokeStyle: "#F99fff"
+//             }
+//         });
+// let boxC  = Bodies.rectangle(200, 100, 50, 20, {
+//     friction: 0.3,
+//     frictionAir: 0.00001,
+//     restitution: 0.8,
+//     render: {
+//         fillStyle: "#F99fff",
+//         strokeStyle: "#F99fff"
+//     }
+// });
+// let AtoB = Constraint.create({
+//     bodyA:boxA,
+//     bodyB:boxB,
+//     bodyC:boxC,
+//     length: 200,
+//     stiffness: 0.5,
+//     pointA: {
+//         x: 0,
+//         y: 40
+//     }
+// })
+// let AtoC = Constraint.create({
+//     bodyA:boxA,
+//     bodyB:boxC,
+//     length: 100,
+//     stiffness: 0.5,
+//     pointC: {
+//         x: 0,
+//         y: 0
+//     }
+// })
+// // let options = {
+// //     bodyA: p1.body,
+// //     bodyB: p2.body,
+// //     length: 50,
+// //     stiffness: 0.4
+// // }
+// // let constraint = Constraint.create(options);
+// Composite.add(engine.world, [boxA, boxB, boxC, AtoB, AtoC]);
+
+//////////////
+////////
+
+///////////
+
+
+var ground = Bodies.rectangle(
+    matterContainer.clientWidth / 2,
+    matterContainer.clientHeight + THICCNESS / 3.5,
+    27184,
+    THICCNESS,
+    { isStatic: true }
+);
+
+let leftWall = Bodies.rectangle(
+    0 - THICCNESS / 2,
+    matterContainer.clientHeight / 2,
+    THICCNESS,
+    matterContainer.clientHeight * 5,
+    {
+        isStatic: true
+    }
+);
+
+let rightWall = Bodies.rectangle(
+    matterContainer.clientWidth + THICCNESS / 2,
+    matterContainer.clientHeight / 2,
+    THICCNESS,
+    matterContainer.clientHeight * 5,
+    { isStatic: true }
+);
+
+// add all of the bodies to the world
+Composite.add(engine.world, [ground, leftWall, rightWall]);
+
+
+
+let mouse = Matter.Mouse.create(render.canvas);
+let mouseConstraint = Matter.MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+        stiffness: 0.2,
+        render: {
+            visible: false
+        }
+    }
+});
+
+Composite.add(engine.world, mouseConstraint);
+
+// allow scroll through the canvas
+mouseConstraint.mouse.element.removeEventListener(
+    "mousewheel",
+    mouseConstraint.mouse.mousewheel
+);
+mouseConstraint.mouse.element.removeEventListener(
+    "DOMMouseScroll",
+    mouseConstraint.mouse.mousewheel
+);
+
+// run the renderer
+Render.run(render);
+
+// create runner
+var runner = Runner.create();
+
+// run the engine
+Runner.run(runner, engine);
+
+
+function createCircle() {
+    let circleDiameter =
+        matterContainer.clientWidth * SVG_WIDTH_AS_PERCENT_OF_CONTAINER_WIDTH;
+    let circle = Bodies.circle(
+        matterContainer.clientWidth / 3,
+        10,
+        circleDiameter / 2,
+        {
+            friction: 0.3,
+            frictionAir: 0.00001,
+            restitution: 0.8,
+            render: {
+                fillStyle: "#ef5566",
+                strokeStyle: "#ef5566"
+            }
+        }
+    );
+    Composite.add(engine.world, circle);
+}
+
+function createSvgBodies() {
+    const paths = document.querySelectorAll(SVG_PATH_SELECTOR);
+    paths.forEach((path, index) => {
+        let vertices = Svg.pathToVertices(path);
+        let scaleFactor =
+            (matterContainer.clientWidth * SVG_WIDTH_AS_PERCENT_OF_CONTAINER_WIDTH) /
+            SVG_WIDTH_IN_PX;
+        vertices = Vertices.scale(vertices, scaleFactor, scaleFactor);
+        let svgBody = Bodies.fromVertices(
+            index * SVG_WIDTH_IN_PX + 200,
+            0,
+            [vertices],
+            {
+                friction: 0.3,
+                frictionAir: 0.00001,
+                restitution: 0.8,
+                render: {
+                    fillStyle: "#ef5566",
+                    strokeStyle: "#ef5566",
+                    lineWidth: 1
+                }
+            }
+        );
+        Composite.add(engine.world, svgBody);
+    });
+}
+
+function scaleBodies() {
+    const allBodies = Composite.allBodies(engine.world);
+
+    allBodies.forEach((body) => {
+        if (body.isStatic === true) return; // don't scale walls and ground
+        const { min, max } = body.bounds;
+        const bodyWidth = max.x - min.x;
+        let scaleFactor =
+            (matterContainer.clientWidth * SVG_WIDTH_AS_PERCENT_OF_CONTAINER_WIDTH) /
+            bodyWidth;
+
+        Body.scale(body, scaleFactor, scaleFactor);
+    });
+}
+
+function handleResize(matterContainer) {
+    // set canvas size to new values
+    render.canvas.width = matterContainer.clientWidth;
+    render.canvas.height = matterContainer.clientHeight;
+
+    // reposition ground
+    Body.setPosition(
+        ground,
+        Vector.create(
+            matterContainer.clientWidth / 2,
+            matterContainer.clientHeight + THICCNESS / 2
+        )
+    );
+
+    // reposition right wall
+    Body.setPosition(
+        rightWall,
+        Vector.create(
+            matterContainer.clientWidth + THICCNESS / 2,
+            matterContainer.clientHeight / 2
+        )
+    );
+
+    scaleBodies();
+}
+
+window.addEventListener("resize", () => handleResize(matterContainer));
+
+
+
+
+
+
+
+
+
+
+//BLOB
 // let section = document.querySelector('.page1');
 // let centerSectionWidth = section.offsetWidth / 2;
 // let centerSectionHeigh = section.offsetHeight / 2;
@@ -125,273 +400,102 @@ gsap.registerPlugin(ScrollTrigger);
 //
 // Composite.add(engine.world, mouseConstraint)
 
-// import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
-const {
-    gsap: {
-        registerPlugin,
-        set,
-        to,
-        timeline,
-        delayedCall,
-        utils: { random }
-    },
-    MorphSVGPlugin,
-    Draggable
-} = window;
 
 
 
-
-// Used to calculate distance of "tug"
-let startX;
-let startY;
-
-const CORD_DURATION = 0.1;
-const INPUT = document.querySelector("#light-mode");
-const ARMS = document.querySelectorAll(".bear__arm");
-const PAW = document.querySelector(".bear__paw");
-const CORDS = document.querySelectorAll(".toggle-scene__cord");
-const HIT = document.querySelector(".toggle-scene__hit-spot");
-const DUMMY = document.querySelector(".toggle-scene__dummy-cord");
-const DUMMY_CORD = document.querySelector(".toggle-scene__dummy-cord line");
-const PROXY = document.createElement("div");
-const endY = DUMMY_CORD.getAttribute("y2");
-const endX = DUMMY_CORD.getAttribute("x2");
-// set init position
-const RESET = () => {
-    set(PROXY, {
-        x: endX,
-        y: endY
-    });
-};
-
-const AUDIO = {
-    BEAR_LONG: new Audio("https://assets.codepen.io/605876/bear-groan-long.mp3"),
-    BEAR_SHORT: new Audio(
-        "https://assets.codepen.io/605876/bear-groan-short.mp3"
-    ),
-    DOOR_OPEN: new Audio("https://assets.codepen.io/605876/door-open.mp3"),
-    DOOR_CLOSE: new Audio("https://assets.codepen.io/605876/door-close.mp3"),
-    CLICK: new Audio("https://assets.codepen.io/605876/click.mp3")
-};
-const STATE = {
-    ON: false,
-    ANGER: 0
-};
-set(PAW, {
-    transformOrigin: "50% 50%",
-    xPercent: -30
-});
-set(".bulb", { z: 10 });
-set(ARMS, {
-    xPercent: 10,
-    rotation: -90,
-    transformOrigin: "100% 50%",
-    yPercent: -2,
-    display: "block"
-});
-const CONFIG = {
-    ARM_DUR: 0.4,
-    CLENCH_DUR: 0.1,
-    BEAR_START: 40,
-    BEAR_FINISH: -55,
-    BEAR_ROTATE: -50,
-    DOOR_OPEN: 25,
-    INTRO_DELAY: 1,
-    BEAR_APPEARANCE: 2,
-    SLAM: 3,
-    BROWS: 4
-};
-set(".bear__brows", { display: "none" });
-set(".bear", {
-    rotate: CONFIG.BEAR_ROTATE,
-    xPercent: CONFIG.BEAR_START,
-    transformOrigin: "50% 50%",
-    scale: 0,
-    display: "block"
-});
-
-RESET();
-
-const CORD_TL = () => {
-    const TL = timeline({
-        paused: false,
-        onStart: () => {
-            // Hook this up to localStorage for jhey.dev
-            STATE.ON = !STATE.ON;
-            INPUT.checked = !STATE.ON;
-            set(document.documentElement, { "--on": STATE.ON ? 1 : 0 });
-            set([DUMMY], { display: "none" });
-            set(CORDS[0], { display: "block" });
-            AUDIO.CLICK.play();
-        },
-        onComplete: () => {
-            // BEAR_TL.restart()
-            set([DUMMY], { display: "block" });
-            set(CORDS[0], { display: "none" });
-            RESET();
-        }
-    });
-    for (let i = 1; i < CORDS.length; i++) {
-        TL.add(
-            to(CORDS[0], {
-                morphSVG: CORDS[i],
-                duration: CORD_DURATION,
-                repeat: 1,
-                yoyo: true
-            })
-        );
-    }
-    return TL;
-};
-
-/**
- * Mess around with the actial input toggling here.
- */
-const BEAR_TL = () => {
-    const ARM_SWING = STATE.ANGER > 4 ? 0.2 : CONFIG.ARM_DUR;
-    const SLIDE = STATE.ANGER > CONFIG.BROWS + 3 ? 0.2 : random(0.2, 0.6);
-    const CLOSE_DELAY = STATE.ANGER >= CONFIG.INTRO_DELAY ? random(0.2, 2) : 0;
-    const TL = timeline({
-        paused: false
-    })
-        .to(".door", {
-            onStart: () => AUDIO.DOOR_OPEN.play(),
-            rotateY: 25,
-            duration: 0.2
-        })
-        .add(
-            STATE.ANGER >= CONFIG.BEAR_APPEARANCE && Math.random() > 0.25
-                ? to(".bear", {
-                    onStart: () => {
-                        if (Math.random() > 0.5) {
-                            // delayedCall(random(0, 1.5), () => {
-                            //   AUDIO[
-                            //     STATE.ANGER >= CONFIG.BROWS && Math.random() > 0.5
-                            //       ? 'BEAR_LONG'
-                            //       : 'BEAR_SHORT'
-                            //   ].play()
-                            // })
-                        }
-                        set(".bear", { scale: 1 });
-                    },
-                    xPercent: CONFIG.BEAR_FINISH,
-                    repeat: 1,
-                    repeatDelay: 1,
-                    yoyo: true,
-                    duration: SLIDE
-                })
-                : () => {}
-        )
-        .to(ARMS, {
-            delay: CLOSE_DELAY,
-            duration: ARM_SWING,
-            rotation: 0,
-            xPercent: 0,
-            yPercent: 0
-        })
-        .to(
-            [PAW, "#knuckles"],
-            {
-                duration: CONFIG.CLENCH_DUR,
-                xPercent: (_, target) => (target.id === "knuckles" ? 10 : 0)
-            },
-            `>-${ARM_SWING * 0.5}`
-        )
-        .to(ARMS, {
-            duration: ARM_SWING * 0.5,
-            rotation: 5
-        })
-        .to(ARMS, {
-            rotation: -90,
-            xPercent: 10,
-            duration: ARM_SWING,
-            onComplete: () => {
-                to(".door", {
-                    onComplete: () => AUDIO.DOOR_CLOSE.play(),
-                    duration: 0.2,
-                    rotateY: 0
-                });
-            }
-        })
-        .to(
-            DUMMY_CORD,
-            {
-                duration: CONFIG.CLENCH_DUR,
-                attr: {
-                    x2: parseInt(endX, 10) + 20,
-                    y2: parseInt(endY, 10) + 60
-
-                }
-            },
-            "<"
-        )
-        .to(
-            DUMMY_CORD,
-            {
-                duration: CONFIG.CLENCH_DUR,
-                attr: {
-                    x2: endX,
-                    y2: endY
-                }
-            },
-            ">"
-        )
-        .to(
-            [PAW, "#knuckles"],
-            {
-                duration: CONFIG.CLENCH_DUR,
-                xPercent: (_, target) => (target.id === "knuckles" ? 0 : -28)
-            },
-            "<"
-        )
-        .add(() => CORD_TL(), "<");
-    return TL;
-};
-
-const IMPOSSIBLE_TL = () =>
-    timeline({
-        onStart: () => set(HIT, { display: "none" }),
-        onComplete: () => {
-            set(HIT, { display: "block" });
-            if (Math.random() > 0) STATE.ANGER = STATE.ANGER + 1;
-            if (STATE.ANGER >= CONFIG.BROWS)
-                set(".bear__brows", { display: "block" });
-        }
-    })
-        .add(CORD_TL())
-        .add(BEAR_TL());
-
-Draggable.create(PROXY, {
-    trigger: HIT,
-    type: "x,y",
-    onPress: (e) => {
-        startX = e.x;
-        startY = e.y;
-        RESET();
-    },
-    onDrag: function () {
-        set(DUMMY_CORD, {
-            attr: {
-                x2: this.x,
-                y2: this.y
-            }
-        });
-    },
-    onRelease: function (e) {
-        const DISTX = Math.abs(e.x - startX);
-        const DISTY = Math.abs(e.y - startY);
-        const TRAVELLED = Math.sqrt(DISTX * DISTX + DISTY * DISTY);
-        to(DUMMY_CORD, {
-            attr: { x2: endX, y2: endY },
-            duration: CORD_DURATION,
-            onComplete: () => {
-                if (TRAVELLED > 50) {
-                    IMPOSSIBLE_TL();
-                } else {
-                    RESET();
-                }
-            }
-        });
-    }
-});
+//LAMP
+// const {
+//     gsap: {set, to, timeline },
+//     MorphSVGPlugin,
+//     Draggable } =
+//     window;
+//
+//
+// // Used to calculate distance of "tug"
+// let startX;
+// let startY;
+//
+// const AUDIO = {
+//     CLICK: new Audio('https://assets.codepen.io/605876/click.mp3') };
+//
+// const STATE = {
+//     ON: false };
+//
+// const CORD_DURATION = 0.1;
+//
+// const CORDS = document.querySelectorAll('.toggle-scene__cord');
+// const HIT = document.querySelector('.toggle-scene__hit-spot');
+// const DUMMY = document.querySelector('.toggle-scene__dummy-cord');
+// const DUMMY_CORD = document.querySelector('.toggle-scene__dummy-cord line');
+// const PROXY = document.createElement('div');
+// // set init position
+// const ENDX = DUMMY_CORD.getAttribute('x2');
+// const ENDY = DUMMY_CORD.getAttribute('y2');
+//
+// const RESET = () => {
+//     set(PROXY, {
+//         x: ENDX,
+//         y: ENDY });
+//
+// };
+//
+// RESET();
+//
+// const CORD_TL = timeline({
+//     paused: true,
+//     onStart: () => {
+//         STATE.ON = !STATE.ON;
+//         set(document.documentElement, { '--on': STATE.ON ? 1 : 0 });
+//         set([DUMMY, HIT], { display: 'none' });
+//         set(CORDS[0], { display: 'block' });
+//         AUDIO.CLICK.play();
+//     },
+//     onComplete: () => {
+//         set([DUMMY, HIT], { display: 'block' });
+//         set(CORDS[0], { display: 'none' });
+//         RESET();
+//     } });
+//
+//
+// for (let i = 1; i < CORDS.length; i++) {
+//     CORD_TL.add(
+//         to(CORDS[0], {
+//             morphSVG: CORDS[i],
+//             duration: CORD_DURATION,
+//             repeat: 1,
+//             yoyo: true }));
+//
+//
+// }
+//
+// Draggable.create(PROXY, {
+//     trigger: HIT,
+//     type: 'x,y',
+//     onPress: e => {
+//         startX = e.x;
+//         startY = e.y;
+//     },
+//     onDrag: function () {
+//         set(DUMMY_CORD, {
+//             attr: {
+//                 x2: this.x,
+//                 y2: this.y } });
+//
+//
+//     },
+//     onRelease: function (e) {
+//         const DISTX = Math.abs(e.x - startX);
+//         const DISTY = Math.abs(e.y - startY);
+//         const TRAVELLED = Math.sqrt(DISTX * DISTX + DISTY * DISTY);
+//         to(DUMMY_CORD, {
+//             attr: { x2: ENDX, y2: ENDY },
+//             duration: CORD_DURATION,
+//             onComplete: () => {
+//                 if (TRAVELLED > 50) {
+//                     CORD_TL.restart();
+//                 } else {
+//                     RESET();
+//                 }
+//             } });
+//
+//     } });
