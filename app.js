c2647b7410cff7d9fe06c7ff01629fad9ba6efa2
Diana-Sh1 <dianaa15052015@gmail.com>
2023-02-07 14:41:27 +0300

    hey making

--- app.js
+++ app.js
@@ -2,162 +2,291 @@
 
 //HELLO
 const THICCNESS = 60;
-const SVG_PATH_SELECTORh = "#matter-pathH";
-const SVG_PATH_SELECTORi = "#matter-pathI";
-
 const SVG_WIDTH_IN_PX = 100;
 const SVG_WIDTH_AS_PERCENT_OF_CONTAINER_WIDTH = 0.1;
 const width = window.innerWidth;
 const height = window.innerHeight;
-const matterContainer = document.querySelector(".grid");
+const matterContainer = document.querySelector(".container-canvas");
 
 // module aliases
-var Engine = Matter.Engine,
-    Render = Matter.Render,
-    Runner = Matter.Runner,
-    Bodies = Matter.Bodies,
-    Composite = Matter.Composite,
-    Composites = Matter.Composites,
-    Common = Matter.Common,
-    Body = Matter.Body,
-    Svg = Matter.Svg,
-    Vector = Matter.Vector,
-    Vertices = Matter.Vertices,
-    Constraint = Matter.Constraint,
-    World = Matter.World;
+
+ let Engine = Matter.Engine,
+        Render = Matter.Render,
+        Runner = Matter.Runner,
+        Bodies = Matter.Bodies,
+        Composite = Matter.Composite,
+        Composites = Matter.Composites,
+        Events = Matter.Events,
+        Body = Matter.Body,
+        Svg = Matter.Svg,
+        Vector = Matter.Vector,
+        Vertices = Matter.Vertices,
+        Constraint = Matter.Constraint,
+        World = Matter.World;
 
 // create an engine
-var engine = Engine.create();
-world = engine.world; //
-
+    let engine = Engine.create({
+        enableSleeping: true
+    });
+    world = engine.world; //
 
 
 // create runner
-var runner = Runner.create();
-Runner.run(runner, engine);
+    var runner = Runner.create();
+    Runner.run(runner, engine);
 
 // add bodies
-var group = Body.nextGroup(true);
+// var group = Body.nextGroup(true);
 
 // create a renderer
-var render = Render.create({
-    element: matterContainer,
-    engine: engine,
-    options: {
-        width: matterContainer.clientWidth,
-        height: matterContainer.clientHeight,
-        background: "transparent",
-        wireframes: false,
-        showAngleIndicator: false,
+    var render = Render.create({
+        element: matterContainer,
+        engine: engine,
+        options: {
+            width: matterContainer.clientWidth,
+            height: matterContainer.clientHeight,
+            background: "transparent",
+            wireframes: false,
+            showAngleIndicator: false,
 
-    }
-});
-Render.run(render);
-
-let ropeSegments = Composites.stack(width / 2, 50, 20, 1, 0, 0, function(x, y) {
-    return Bodies.rectangle(x, y, 10, 10, {
-        density: 1, // really high density but you get the idea
-        isSensor: true,
-        frictionAir: 0.0005
+        }
     });
-});
-////#2
+    Render.run(render);
 
-///
+//creating segments for rope
+    let ropeSegments = Composites.stack(width / 2, 50, 20, 1, 0, 0, function (x, y) {
+        return Bodies.rectangle(x, y, 10, 10, {
+            density: 1, // really high density but you get the idea
+            isSensor: true,
+            frictionAir: 0.0005,
+            render: {
+
+            }
+        });
+    });
+//////
+//     let ropeSegments2 = Composites.stack(width / 2, 50, 20, 1, 0, 0, function (x, y) {
+//         return Bodies.rectangle(x, y, 10, 10, {
+//             density: 1, // really high density but you get the idea
+//             isSensor: true,
+//             frictionAir: 0.0005
+//         });
+//     });
+////
+//     let ropeSegments3 = Composites.stack(width / 2, 50, 20, 1, 0, 0, function (x, y) {
+//         return Bodies.rectangle(x, y, 10, 10, {
+//             density: 1, // really high density but you get the idea
+//             isSensor: true,
+//             frictionAir: 0.0005
+//         });
+//     });
 
 // // chain Stack together
-let rope = Composites.chain(ropeSegments, 0, 0.5, 0, -0.5, {
-    length: 0,
-    stiffness: 1,
-    friction: 0,
-    frictionAir: 0,
-    frictionStatic: 0,
-    render: {
-        lineWidth: 0,
-        anchors: false
-    }
-});
+    let rope = Composites.chain(ropeSegments, 0, 0.5, 0, -0.5, {
+        length: 0,
+        stiffness: 0.5,
+        friction: 0,
+        frictionAir: 0,
+        frictionStatic: 0,
+        render: {
+            lineWidth: 0,
+            anchors: false
+        }
+    });
 ///////
-
+//     let rope2 = Composites.chain(ropeSegments2, 0, 0.5, 0, -0.5, {
+//         length: 0,
+//         stiffness: 1,
+//         friction: 0,
+//         frictionAir: 0,
+//         frictionStatic: 0,
+//         render: {
+//             lineWidth: 0,
+//             anchors: false
+//         }
+//     });
+// ///////
+//     let rope3 = Composites.chain(ropeSegments3, 0, 0.5, 0, -0.5, {
+//         length: 0,
+//         stiffness: 1,
+//         friction: 0,
+//         frictionAir: 0,
+//         frictionStatic: 0,
+//         render: {
+//             lineWidth: 0,
+//             anchors: false
+//         }
+//     });
 // top anchor
-Composite.add(ropeSegments, Constraint.create({
-    pointA: { x: width / 2 + 400, y: 0 },
-    bodyB: ropeSegments.bodies[0],
-    pointB: { x: 0, y: -5 },
-    length: 0,
-    stiffness: 1,
-    friction: 0,
-    frictionAir: 0,
-    frictionStatic: 0,
-    render: {
-        lineWidth: 0,
-        anchors: false
-    }}));
+    Composite.add(ropeSegments,
+        Constraint.create({
+            bodyB: ropeSegments.bodies[0],
+            pointA: {x: width / 1.3 , y: 0},
+            pointB: {x: 0, y: -5},
+            length: 0,
+            stiffness: 1,
+            friction: 0,
+            frictionAir: 0,
+            frictionStatic: 4,
+            render: {
+                lineWidth: 0,
+                anchors: false
+            }
+        }));
 
 ///////////
-
+//     Composite.add(ropeSegments2,
+//         Constraint.create({
+//             bodyB: ropeSegments2.bodies[0],
+//             pointA: {x: width / 2 + 650, y: 0},
+//             pointB: {x: 0, y: -5},
+//             length: 0,
+//             stiffness: 1,
+//             friction: 0,
+//             frictionAir: 0,
+//             frictionStatic: 4,
+//             render: {
+//                 lineWidth: 0,
+//                 anchors: false
+//             }
+//         }));
 //////
+//     Composite.add(ropeSegments3,
+//         Constraint.create({
+//             bodyB: ropeSegments3.bodies[0],
+//             pointA: {x: width / 2 + 700, y: 0},
+//             pointB: {x: 0, y: -5},
+//             length: 0,
+//             stiffness: 1,
+//             friction: 0,
+//             frictionAir: 0,
+//             frictionStatic: 4,
+//             render: {
+//                 lineWidth: 0,
+//                 anchors: false
+//             }
+//         }));
+///
 
+    World.add(world, rope);
 
-World.add(world, rope);
+    Render.lookAt(render, {
+        min: {x: 0, y: 0},
+        max: {x: width, y: height}
+    });
 
-Render.lookAt(render, {
-    min: { x: 0, y: 0 },
-    max: { x: width, y: height }
-});
-
-let x = ropeSegments.bodies[ropeSegments.bodies.length - 1].position.x;
-let y = ropeSegments.bodies[ropeSegments.bodies.length - 1].position.y;
+    let x = ropeSegments.bodies[ropeSegments.bodies.length - 1].position.x;
+    let y = ropeSegments.bodies[ropeSegments.bodies.length - 1].position.y;
 /////
-// let x1 = ropeSegments1.bodies[ropeSegments1.bodies.length - 1].position.x;
-// let y2 = ropeSegments1.bodies[ropeSegments1.bodies.length - 1].position.y;
-
-let pixelHeight = 0.2 * height;
+    let pixelHeight = 0.2 * height;
 
 /////////////
-const pathH = document.querySelector('#matter-pathH');
-// const pathI = document.querySelector('#matter-pathI');
+    const pathH = document.querySelector('#matter-pathH');
+    const pathE = document.querySelector('#matter-pathE');
+    const pathY = document.querySelector('#matter-pathY');
 
-let vertices = Svg.pathToVertices(pathH);
-let scaleFactor =
-    (matterContainer.clientWidth * SVG_WIDTH_AS_PERCENT_OF_CONTAINER_WIDTH) /
-    SVG_WIDTH_IN_PX;
-vertices = Vertices.scale(vertices, scaleFactor, scaleFactor);
-let svgBody = Bodies.fromVertices(
-    100,
-    0,
-    [vertices], {
-        friction: 1,
-        frictionAir: 0.00001,
-        restitution: 0.8,
-        render: {
-            fillStyle: "#f7b731",
-            strokeStyle: "#f7b731",
-            lineWidth: 1
+picture = Bodies.rectangle(x, y, pixelHeight *0.724, pixelHeight, {
+    density: 0.01704 / height,
+    render: {
+        sprite: {
+            texture: "img/hey2.svg"
         }
     }
-);
 
-// Composite.add(engine.world, svgBody);
-
+})
+    // let vertices = Svg.pathToVertices(pathH);
+    let scaleFactor =
+        (matterContainer.clientWidth * SVG_WIDTH_AS_PERCENT_OF_CONTAINER_WIDTH) /
+        SVG_WIDTH_IN_PX;
+    // vertices = Vertices.scale(vertices, scaleFactor, scaleFactor);
+    // let svgBody = Bodies.fromVertices(
+    //     100,
+    //     0,
+    //     [vertices], {
+    //         // friction: 1,
+    //         // frictionAir: 0.00001,
+    //         // restitution: 0.8,
+    //         render: {
+    //             fillStyle: "#f7b731",
+    //             strokeStyle: "#f7b731",
+    //             lineWidth: 1
+    //         }
+    //     }
+    // );
+////
+//     let vertices2 = Svg.pathToVertices(pathE);
+//     vertices2 = Vertices.scale(vertices2, scaleFactor, scaleFactor);
+//     let svgBody2 = Bodies.fromVertices(
+//         100,
+//         0,
+//         [vertices2], {
+//             friction: 1,
+//             frictionAir: 0.00001,
+//             restitution: 0.8,
+//             render: {
+//                 fillStyle: "#f7b731",
+//                 strokeStyle: "#f7b731",
+//                 lineWidth: 1
+//             }
+//         }
+//     );
 //////
+//     let vertices3 = Svg.pathToVertices(pathY);
+//     vertices3 = Vertices.scale(vertices3, scaleFactor, scaleFactor);
+//     let svgBody3 = Bodies.fromVertices(
+//         100,
+//         0,
+//         [vertices3], {
+//             friction: 1,
+//             frictionAir: 0.00001,
+//             restitution: 0.8,
+//             render: {
+//                 fillStyle: "#f7b731",
+//                 strokeStyle: "#f7b731",
+//                 lineWidth: 1
+//             }
+//         }
+//     );
 
-
-
-Composite.add(ropeSegments, Constraint.create({
-    bodyA: ropeSegments.bodies[ropeSegments.bodies.length-1],
-    bodyB: svgBody,
-    length: 0,
-    angularStiffness: 1,
-    render: {
-        lineWidth: 0
-    }
-}));
-
-World.add(world, svgBody);
+/////
+    Composite.add(ropeSegments, Constraint.create({
+        pointA: {x: 0 , y: 50},
+        bodyA: ropeSegments.bodies[ropeSegments.bodies.length - 1],
+        bodyB: picture,
+        length: 0,
+        angularStiffness: 1,
+        render: {
+            lineWidth: 0
+        }
+    }));
+////
+//     Composite.add(ropeSegments2,
+//         Constraint.create(
+//             {
+//                 pointA: {x: -50 , y: 50},
+//                 bodyA: ropeSegments2.bodies[ropeSegments2.bodies.length - 1],
+//                 bodyB: picture,
+//                 length: 0,
+//                 angularStiffness: 1,
+//                 render: {
+//                     lineWidth: 0
+//                 }
+//             }));
+/////
+//     Composite.add(ropeSegments3,
+//         Constraint.create(
+//             {
+//                 bodyA: ropeSegments3.bodies[ropeSegments3.bodies.length - 1],
+//                 bodyB: svgBody3,
+//                 length: 0,
+//                 angularStiffness: 1,
+//                 render: {
+//                     lineWidth: 0
+//                 }
+//             }));
+World.add(world, picture);
+    // World.add(world, [svgBody, svgBody2, svgBody3]);
 ///////////////
-
-
 
 
 // createSvgBodies();
@@ -182,9 +311,6 @@ World.add(world, svgBody);
 //     );
 //     Composite.add(engine.world, circle);
 // }
-
-
-
 
 
 // function createSvgBodies() {
@@ -214,124 +340,124 @@ World.add(world, svgBody);
 // }
 
 
-var ground = Bodies.rectangle(
-    matterContainer.clientWidth / 2,
-    matterContainer.clientHeight + THICCNESS / 3.5,
-    27184,
-    THICCNESS,
+    var ground = Bodies.rectangle(
+        matterContainer.clientWidth / 2,
+        matterContainer.clientHeight + THICCNESS / 3.5,
+        27184,
+        THICCNESS,
 
-    { isStatic: true,
-    render: {
-        fillStyle: '#6D214F'
-    }},
+        {
+            isStatic: true,
+            render: {
+                fillStyle: '#6D214F'
+            }
+        },
+    );
 
-);
-
-let leftWall = Bodies.rectangle(
-    0 - THICCNESS / 2,
-    matterContainer.clientHeight / 2,
-    THICCNESS,
-    matterContainer.clientHeight * 5,
-    {
-        isStatic: true,
-        render: {
-            fillStyle: 'transparent'
+    let leftWall = Bodies.rectangle(
+        0 - THICCNESS / 2,
+        matterContainer.clientHeight / 2,
+        THICCNESS,
+        matterContainer.clientHeight * 5,
+        {
+            isStatic: true,
+            render: {
+                fillStyle: 'transparent'
+            }
         }
-    }
-);
+    );
 
-let rightWall = Bodies.rectangle(
-    matterContainer.clientWidth + THICCNESS / 2,
-    matterContainer.clientHeight / 2,
-    THICCNESS,
-    matterContainer.clientHeight * 5,
-    { isStatic: true,
-    render: {
-        fillStyle: 'transparent'
-    }}
-);
-
-Composite.add(engine.world, [ground, leftWall, rightWall]);
-
-
-
-let mouse = Matter.Mouse.create(render.canvas);
-let mouseConstraint = Matter.MouseConstraint.create(engine, {
-    mouse: mouse,
-    constraint: {
-        stiffness: 0.2,
-        render: {
-            visible: false
+    let rightWall = Bodies.rectangle(
+        matterContainer.clientWidth + THICCNESS / 2,
+        matterContainer.clientHeight / 2,
+        THICCNESS,
+        matterContainer.clientHeight * 5,
+        {
+            isStatic: true,
+            render: {
+                fillStyle: 'transparent'
+            }
         }
-    }
-});
+    );
 
-Composite.add(engine.world, mouseConstraint);
+    Composite.add(engine.world, [ground, leftWall, rightWall]);
+
+
+    let mouse = Matter.Mouse.create(render.canvas);
+    let mouseConstraint = Matter.MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+            stiffness: 0.2,
+            render: {
+                visible: false
+            }
+        }
+    });
+
+    Composite.add(engine.world, mouseConstraint);
 
 // allow scroll through the canvas
-mouseConstraint.mouse.element.removeEventListener(
-    "mousewheel",
-    mouseConstraint.mouse.mousewheel
-);
-mouseConstraint.mouse.element.removeEventListener(
-    "DOMMouseScroll",
-    mouseConstraint.mouse.mousewheel
-);
-
-//run the renderer
-Render.run(render);
-
-// create runner
-var runner = Runner.create();
-
-// run the engine
-Runner.run(runner, engine);
-
-
-
-function scaleBodies() {
-    const allBodies = Composite.allBodies(engine.world);
-
-    allBodies.forEach((body) => {
-        if (body.isStatic === true) return; // don't scale walls and ground
-        const { min, max } = body.bounds;
-        const bodyWidth = max.x - min.x;
-        let scaleFactor =
-            (matterContainer.clientWidth * SVG_WIDTH_AS_PERCENT_OF_CONTAINER_WIDTH) /
-            bodyWidth;
-
-        Body.scale(body, scaleFactor, scaleFactor);
-    });
-}
-
-function handleResize(matterContainer) {
-    // set canvas size to new values
-    render.canvas.width = matterContainer.clientWidth;
-    render.canvas.height = matterContainer.clientHeight;
-
-    // reposition ground
-    Body.setPosition(
-        ground,
-        Vector.create(
-            matterContainer.clientWidth / 2,
-            matterContainer.clientHeight + THICCNESS / 2
-        )
+    mouseConstraint.mouse.element.removeEventListener(
+        "mousewheel",
+        mouseConstraint.mouse.mousewheel
+    );
+    mouseConstraint.mouse.element.removeEventListener(
+        "DOMMouseScroll",
+        mouseConstraint.mouse.mousewheel
     );
 
-    // reposition right wall
-    Body.setPosition(
-        rightWall,
-        Vector.create(
-            matterContainer.clientWidth + THICCNESS / 2,
-            matterContainer.clientHeight / 2
-        )
-    );
+// //run the renderer
+// Render.run(render);
+//
+// // create runner
+// var runner = Runner.create();
+//
+// // run the engine
+// Runner.run(runner, engine);
 
-    scaleBodies();
-}
 
-window.addEventListener("resize", () => handleResize(matterContainer));
+    function scaleBodies() {
+        const allBodies = Composite.allBodies(engine.world);
 
+        allBodies.forEach((body) => {
+            if (body.isStatic === true) return; // don't scale walls and ground
+            const {min, max} = body.bounds;
+            const bodyWidth = max.x - min.x;
+            let scaleFactor =
+                (matterContainer.clientWidth * SVG_WIDTH_AS_PERCENT_OF_CONTAINER_WIDTH) /
+                bodyWidth;
+
+            Body.scale(body, scaleFactor, scaleFactor);
+        });
+    }
+
+    function handleResize(matterContainer) {
+        // set canvas size to new values
+        render.canvas.width = matterContainer.clientWidth;
+        render.canvas.height = matterContainer.clientHeight;
+
+        // reposition ground
+        Body.setPosition(
+            ground,
+            Vector.create(
+                matterContainer.clientWidth / 2,
+                matterContainer.clientHeight + THICCNESS / 2
+            )
+        );
+
+        // reposition right wall
+        Body.setPosition(
+            rightWall,
+            Vector.create(
+                matterContainer.clientWidth + THICCNESS / 2,
+                matterContainer.clientHeight / 2
+            )
+        );
+
+        scaleBodies();
+    }
+
+    window.addEventListener("resize", () => handleResize(matterContainer));
 
 
 
